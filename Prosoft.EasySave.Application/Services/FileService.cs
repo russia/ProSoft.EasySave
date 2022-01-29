f6728e9185444b00b4b07dff2ee483198e36673a
Russia <61105546+Russia@users.noreply.github.com>
2022-01-29 20:19:30 +0100

    Implement the logging service #2
    Adding Serilog as a logger service.
    Improved the checksum calculation method.
    
    https://github.com/russia/ProSoft.EasySave/issues/2

--- Prosoft.EasySave.Application/Services/FileService.cs
+++ Prosoft.EasySave.Application/Services/FileService.cs
@@ -1,20 +1,21 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
 using ProSoft.EasySave.Application.Enums;
 using ProSoft.EasySave.Application.Extensions;
 using ProSoft.EasySave.Application.Interfaces.Services;
 using ProSoft.EasySave.Application.Models.Contexts;
-using System.Diagnostics;
-using System.Security.Cryptography;
+using ProSoft.EasySave.Application.Models.Logging;
+using Serilog;
 
 namespace ProSoft.EasySave.Application.Services;
 
 public class FileService : IFileService
 {
-    private readonly ILogger<SampleService> _logger;
+    private readonly ILogger _logger;
 
-    public FileService(ILogger<SampleService> logger)
+    public FileService(ILogger logger)
     {
-        _logger = logger;
+        _logger = logger.ForContext<FileService>();
     }
 
     public async Task<JobResult> CopyFiles(JobContext jobContext, CancellationToken cancellationToken = default)
@@ -22,7 +23,7 @@ public class FileService : IFileService
         var stopWatch = new Stopwatch();
         stopWatch.Start();
         jobContext.StateType = StateType.PROCESSING;
-        _logger.LogInformation($"[{jobContext.Name}] is processing.");
+        Console.WriteLine($"[{jobContext.Name}] is processing.");
 
         var sourceDir = new DirectoryInfo(jobContext.SourcePath);
         var destinationDir = new DirectoryInfo(jobContext.DestinationPath);
@@ -36,23 +37,37 @@ public class FileService : IFileService
             .ToList();
 
         if (jobContext.TransferType is TransferType.DIFFERENTIAL)
-        {
-            sourceFiles.RemoveAll(srcFile => destinationFiles.Any(destFile => destFile.GetHash() == srcFile.GetHash()));
-        }
+            sourceFiles.RemoveAll(srcFile => 
+                destinationFiles.Any(destFile => srcFile.Length == destFile.Length && Task.Run(() =>
+                                                           destFile.Compare(srcFile), cancellationToken).Result));
 
         foreach (var sourceFile in sourceFiles)
         {
-            cancellationToken.ThrowIfCancellationRequested();
+            cancellationToken.ThrowIfCancellationRequested(); // TODO : handle cancellation tokens.
             var destinationPath = Path.Combine(jobContext.DestinationPath, sourceFile.Name);
+            var stopwatchFile = new Stopwatch();
+            stopwatchFile.Start();
             sourceFile.CopyTo(destinationPath, true);
-            _logger.LogInformation($"[{jobContext.Name}] File {sourceFile.Name} copied to {destinationPath}.");
+            stopwatchFile.Stop();
+            Console.WriteLine($"[{jobContext.Name}] File {sourceFile.Name} copied to {destinationPath}.");
+            var jsonLog = new LogEntry
+            {
+                Name = jobContext.Name,
+                FileSource = sourceFile.FullName,
+                FileTarget = destinationPath,
+                FileSize = sourceFile.Length,
+                FileTransferTime = stopwatchFile.ElapsedMilliseconds,
+                Time = DateTime.UtcNow
+            };
+
+            _logger.Information(JsonConvert.SerializeObject(jsonLog, Formatting.Indented));
         }
 
         jobContext.StateType = StateType.COMPLETED;
-        _logger.LogInformation($"[{jobContext.Name}] is completed.");
+        Console.WriteLine($"[{jobContext.Name}] is completed.");
         stopWatch.Stop();
 
-        return new JobResult()
+        return new JobResult
         {
             Name = jobContext.Name,
             FilesNumber = sourceFiles.Count,
