22da85bafac921cc60549f3a86cdfe2005163556
ymonnet <Yanis.monnet1@gmail.com>
2022-02-07 17:24:29 +0100

    init moderwpf

--- ProSoft.EasySave.Presentation/Views/MainWindow.xaml.cs
+++ ProSoft.EasySave.Presentation/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using ProSoft.EasySave.Presentation.Views.PartialViews;
+using System.Windows;
+
 
 namespace ProSoft.EasySave.Presentation.Views {
 
@@ -12,5 +14,21 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+        public void NavigationView_SelectionChanged5(ModernWpf.Controls.NavigationView sender, ModernWpf.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            /*if (args.IsSettingsSelected)
+            {
+                contentFrame5.Navigate(typeof(SampleSettingsPage));
+            }
+            else
+            {*/
+                var selectedItem = (ModernWpf.Controls.NavigationViewItem)args.SelectedItem;
+                string selectedItemTag = (string)selectedItem.Tag;
+                sender.Header = "Sample Page " + selectedItemTag.Substring(selectedItemTag.Length - 1);
+                string pageName = "SamplesCommon.SamplePages." + selectedItemTag;
+                var pageType = typeof(_SaveView).Assembly.GetType(pageName);
+                contentFrame5.Navigate(pageType);
+            //}
+        }
     }
 }
